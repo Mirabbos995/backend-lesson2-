26de1d6e1cc596207b9c05a6ebf503bbdc5b44ce
Mirabbos995 <jahonaliyevmirabbos621@gmail.com>
2024-02-10 09:05:06 +0300

    project

---
+++ main.py
@@ -0,0 +1,124 @@
+# a = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"
+# b = a.split()
+# print(len(b))
+
+
+# a = input("Enter the word: ")         # 1 - misol
+# b = a[0:2]
+#
+# print(f"{b}..  {b}..  {a}")
+
+
+# a = input("Enter: ")     # 2 - misol
+# b = "Luke"
+#
+# if a == "Darth Vader":
+#     print(f"{b}, I am your father")
+
+
+# a = input("Enter: ")          # 3 - misol
+# def feeling():
+#     if a == "happy":
+#         print(f"Today I'm feeling {a}")
+#     elif a == "sad":
+#         print(f"Today I'm feeling {a}")
+#     elif a == "":
+#         print("Today I'm feeling neutral")
+# feeling()
+
+
+# How Many Vowels?        # 4 - misol
+
+# not found
+
+
+
+# charcode = input("Enter: ")              # 5 - misol
+#
+# if charcode == "A":
+#     print(97)
+# elif charcode == "a":
+#     print(65)
+
+
+
+
+# Find ASCII Charcode of Inverse Case Character    6 - misol
+
+# not done
+
+
+
+
+# def inequality():
+#     a = int(input("Enter the number: "))  # 7 - misol
+#     b = int(input("Enter the number: "))
+#     c = int(input("Enter the number: "))
+#     d = a > b > c or a < b < c or a == b == c or a > b < c
+#     print(d)
+# inequality()
+
+
+# def num():
+#     vowel = input("Enter: ")  # 8 - misol
+#
+#     b = vowel.replace("a", "*")
+#     print(b)
+# num()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# b = "Eminem"
+# a = b.casefold()    # hamma harfni kichkina qiladi
+# print(a)
+
+
+
+# b = "Eminem"
+# a = b.endswith("Eminem")    # "Eminem" bo'lsa True, boshqa harflar bo'lsa False
+# print(a)
+
+
+# b = "Eminem"
+# a = b.encode()    # b'Eminem' degan yozuvni chiqaradi
+# print(a)
+
+# b = "Eminem"
+# a = b.expandtabs()    #Eminem ni o'zini chiqradi
+# print(a)
+
+
+# b = "Eminem"
+# a = b.isdecimal()    # True
+# print(a)
+
+# b = "Eminem"
+# a = b.format("Eminem")    # Eminem ni chiqaradi
+# print(a)
+
+# b = "Eminem"
+# a = b.isprintable()    # True
+# print(a)
